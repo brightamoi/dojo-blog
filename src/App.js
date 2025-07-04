58e9cfe4d59c724404f0b2f737345e2e947273c9
brightamoi <amoi.bright92@gmail.com>
2025-07-04 15:36:42 -0400

    Handle delete

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "welcome to the new blog";
-  const likes = 50;
+  // const title = "welcome to the new blog";
+  // const likes = 50;
   return (
     <div className="App">
       <Navbar />
